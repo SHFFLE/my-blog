af1611a8547de63796d27bc020c428af1bd5507c
SHFFLE <mistersh1ffle@gmail.com>
2017-01-04 11:07:18 -0600

    Initial commit

---
+++ src/main/java/com/codeup/MathController.java
@@ -0,0 +1,26 @@
+package com.codeup;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class MathController {
+
+    @GetMapping("/math/{x}/{operator}/{y}")
+    @ResponseBody
+    public String math(@PathVariable int x, @PathVariable String operator, @PathVariable int y) {
+        switch (operator){
+            case "plus":
+                return ("Result: " + (x + y));
+            case "minus":
+                return ("Result: " + (x - y));
+            case "times":
+                return ("Result: " + (x * y));
+            case "divided":
+                return ("Result: " + (x / y));
+        }
+        return ("Valid operators are plus, minus, times, and divided.");
+    }
+}
